13758b9f082912bc36372ed312bf4f23c62354e7
Molinar <502341121@logon.ds.ge.com>
2017-03-10 13:07:15 +0100

    Adicionando logica para protecao da pagina manage-exercises.

--- app/src/js/fitqui.js
+++ app/src/js/fitqui.js
@@ -15,9 +15,27 @@ var _fitqui = (function(){
     naoRepetirAvisoDeCuidado : "naoRepetirAvisoDeCuidado",
     naoRepetirAvisoDeCuidadoData : "naoRepetirAvisoDeCuidadoData"
   };
+  var analyticsException = function(msg,fatal){
+    ga('send','exception',{
+      'exDescription': msg,
+      'exFatal': fatal
+    });
+  };
+  var setSpinner = function(){
+    window.scrollTo(0,0);
+    var spinner = document.querySelector('fitqui-spinner#spinner');
+    spinner.setAttribute("ativado",true);
+  };
+  var unsetSpinner = function(){
+    var spinner = document.querySelector('fitqui-spinner#spinner');
+    spinner.removeAttribute("ativado");
+  };
   return {
     devMode: devMode,
     log: log,
-    storageVariableNames: storageVariableNames
+    storageVariableNames: storageVariableNames,
+    analyticsException: analyticsException,
+    setSpinner: setSpinner,
+    unsetSpinner: unsetSpinner
   }
 })();
