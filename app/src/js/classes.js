cac782ed3a9f3096b0ba9a6d6daaa8997afde1b0
Molinar <502341121@logon.ds.ge.com>
2017-03-13 19:32:50 +0100

    Mudando estrutura do BD para refletir niveis de acesso e criando as regras de acesso.

--- app/src/js/classes.js
+++ app/src/js/classes.js
@@ -5,10 +5,6 @@ var _fitquiClasses = (function(){
     *Os itens que estão comentados é por que não são usados no front-end,
     *apenas no banco de dados
     */
-    //Usuários que curtiram o exercício
-    //curtidores = [],
-    //Usuários que não gostaram do exercício
-    //detratores = [],
     //Nível de dificuldade (medido de 1 a 9) para realizar 10 repetições de um exercício de força ou 10 minutos de um aeróbico
     //dificuldade = 0,
     //Caminho para o gif
@@ -19,14 +15,6 @@ var _fitquiClasses = (function(){
     musculosSecundarios = ["Triceps","Antebraço"],
     //Nome do exercício
     nome = "Exercício",
-    //Notas positivas recebidas
-    //notaPositiva = 0,
-    //Notas neutras recebidas
-    //notaNeutra = 0,
-    //Notas negativas recebidas
-    //notaNegativa = 0,
-    //Nota "quasi-NPS" do exercício. Calculada da seguinte forma: (positivas - negativas)/(positivas+neutras+negativas)
-    //nps = 0,
     //Tipo do exercício: [Força, Aeróbico, Aquecimento, Alongamento ...]
     tipo = "Força",
     //Equipamento necessário para realizar o exercício
@@ -35,8 +23,6 @@ var _fitquiClasses = (function(){
     //local = ['casa','academia'],
     //Link do youtube que mostra alguém realizando o exercício
     youtube = "https://www.youtube.com/watch?v=C8ptU8OJvCM",
-    //Indicador mostrando se o vídeo relacionado ao exercício é domínio público
-    //youtubeAcessivel = true
     //Número de repetições para exercícios de força para nivel iniciante. Igual a 0 quando aeróbico
     repeticoes = 3,
     //Duração do exercício aeróbico para nivel iniciante. Quando exercício de força, indica o tempo (em segundos) para realizar o numero de repeticoes
@@ -51,6 +37,31 @@ var _fitquiClasses = (function(){
       youtube: youtube,
       repeticoes: repeticoes,
       duracao: duracao
+    }
+  };
+  var criarExercicioInfo = function(
+    /*
+    *Cada ítem abaixo tem uma explicação em cima
+    *Os itens que estão comentados é por que não são usados no front-end,
+    *apenas no banco de dados
+    */
+    //Usuários que curtiram o exercício
+    //curtidores = [],
+    //Usuários que não gostaram do exercício
+    //detratores = [],
+    //Notas positivas recebidas
+    //notaPositiva = 0,
+    //Notas neutras recebidas
+    //notaNeutra = 0,
+    //Notas negativas recebidas
+    //notaNegativa = 0,
+    //Nota "quasi-NPS" do exercício. Calculada da seguinte forma: (positivas - negativas)/(positivas+neutras+negativas)
+    //nps = 0,
+    //Indicador mostrando se o vídeo relacionado ao exercício é domínio público
+    //youtubeAcessivel = true
+  ){
+    return{
+
     }
   };
   var criarSerie = function(
